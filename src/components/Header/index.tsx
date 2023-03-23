7ed74d4e49ec32b227c207cc5cba1071d7ce62e6
Eduardo <eduardo.sorrentino5@gmail.com>
2023-03-23 11:23:01 -0300

    fix erros context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import * as Dialog from '@radix-ui/react-dialog';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
-import { NewTransactionModal } from "../NewTransactionModal";
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -11,13 +11,11 @@ export function Header() {
         <img src={logoImg} alt="" />
 
         <Dialog.Root>
-
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
           <NewTransactionModal />
-
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
