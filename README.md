16970b1fe0df3ce85a9bfd20582a6967910389d0
Eduardo <eduardo.sorrentino5@gmail.com>
2023-03-23 10:01:14 -0300

    aplicacao finalizada

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center" >DT Money</h1>
+
+Aplicação para regular as transações de entrada e saída para controlar as finanças, com o título, o valor, a categoria e a data da transação.
+<p>Foi feita com Typescript, Styled Components, React e algumas bibliotecas auxiliares como: React Hook Form, Date-fns, Immer, Zod, Phosphor e Radix.</p>
+
+## Execução do projeto:
+
+Para rodar o projeto, primeiro é necessário clonar o repositório e abrir em uma IDE, VSCode por exemplo.
+
+```bash
+# Clonar repositório
+$ git clone https://github.com/Edusorrentino10/DT-Money-Atualizado.git
+# Acessar pasta do projeto clonado
+$ cd DT-Money-Atualizado
+
+# Instalar dependências
+$ npm install
+
+# Executar aplicação
+$ npm run dev
+
+# Abrir aplicação na porta 5173, acessando: http://localhost:5173
+```
+
+<!-- <h2 align="center"><img src="./src/assets/dtmoney-print-home.png"></h2> -->
+<!-- <h2 align="center"><img src="./src/assets/dtmoney-print-transaction.png"></h2> -->
+
+
+
+Feito por <a href="https://github.com/Edusorrentino10">Eduardo Sorrentino</a>.
+
+Linkedin: https://linkedin.com/in/eduardo-sorrentino/
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,20 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+import { useContextSelector } from 'use-context-selector'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
+
   return (
     <div>
       <Header />
@@ -13,29 +24,26 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">
-                  R$ 12.000,00
-                </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>18/03/2023</td>
-            </tr>
-            <tr>
-              <td width='50%'>Hamburger</td>
-              <td>
-                <PriceHighLight variant="outcome">
-                  - R$ 59,00
-                </PriceHighLight>
-              </td>
-                  <td>Alimentacao</td>
-                  <td>10/03/2023</td>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
-              </tbody>
-            </TransactionsTable>
-          </TransactionsContainer>
-        </div>
-        )
+              )
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
